d4a56d09b8ff78f3f6a5c3f572aba17b3d303ebb
Gorosss <90179487+Gorosss@users.noreply.github.com>
2024-04-17 17:37:59 +0200

    chatUpdate

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,48 +1,134 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { View, Text, TextInput, Button, ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect, useState, useContext, useRef } from 'react';
+import { View, Text, TextInput, Button, ScrollView, StyleSheet, Modal } from 'react-native';
 import io from 'socket.io-client';
 import { UserContext } from '../context/UserContext.jsx';
-import { logoutStorage } from './LoginStorage.jsx'
+import { logoutStorage } from './LoginStorage.jsx';
+import GroupModal from './GroupModal.jsx'; // Importa el componente de popup para crear nuevo grupo o unirse a uno existente
 
 const App = () => {
   const { user, setUser } = useContext(UserContext);
   const { username, userId } = user;
 
   const [groupMessages, setGroupMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
+  const [currentGroupId, setCurrentGroupId] = useState('');
+  const [showGroupModal, setShowGroupModal] = useState(false); // Estado para controlar la visibilidad del popup de nuevo grupo o unión a grupo
+  const [groupModelMode, setGroupModelMode] = useState('create'); // Estado para controlar el modo del popup de nuevo grupo o unión a grupo
+  const socket = useRef(null);
 
-  const socket = io('http://localhost:3000', {
-    auth: {
-      userId: userId,
-      username: username,
-      serverOffset: 0
-    }
-  });
+
 
   useEffect(() => {
-    socket.on('chat message', (msg, serverOffset, username) => {
-      console.log(msg, serverOffset, username)
-      setGroupMessages(prevMessages => [...prevMessages, { msg }]);
-      socket.auth.serverOffset = serverOffset;
+
+    socket.current = io('http://localhost:3000', {
+      auth: {
+        userId: userId,
+        username: username,
+        serverOffset: 0
+      }
     });
 
+
+    socket.current.on('chat message', (msg, serverOffset, username) => {
+
+      setGroupMessages(prevMessages => prevMessages.concat(msg));
+      if (msg.length != 0) {
+        setMessages(msg[0].messages)
+      }
+      console.log("groupMessages", groupMessages)
+    });
+
+
+    socket.current.on('chat new message', (msg, serverOffset, username, groupMessagesEdit) => {
+
+      console.log(groupMessagesEdit)
+      let msgFormated = { id: msg.id, content: msg.content, user_id: msg.user_id, users: { username: username } };
+      groupMessagesEdit.find(group => group.groupId === msg.group_id).messages.push(msgFormated);
+      setGroupMessages(groupMessagesEdit);
+      setMessages(prevMessages => prevMessages.concat(msgFormated))
+
+
+
+    });
+
+    socket.current.on('join group', (msg, username, groupMessagesEdit) => {
+
+      console.log("aaaaaaaaaaaaaaddd",msg)
+
+      setGroupMessages(prevMessages => prevMessages.concat(msg));
+
+      console.log("groupMessages", groupMessages)
+    });
+
+
+    socket.current.on('create group', (msg, username, groupMessagesEdit) => {
+
+      console.log("aaaaaaaaaaaaaaddd",msg)
+
+      setGroupMessages(prevMessages => prevMessages.concat(msg));
+
+      console.log("groupMessages", groupMessages)
+    });
+
+
     return () => {
-      socket.disconnect();
+      socket.current.disconnect();
     };
   }, [username]);
 
+
+
+
   const sendMessage = () => {
     if (inputMessage.trim()) {
-      socket.emit('chat message', inputMessage);
+      socket.current.emit('chat new message', inputMessage, currentGroupId, userId, groupMessages);
       setInputMessage('');
     }
   };
 
   const handleLogout = () => {
-    socket.disconnect();
+    socket.current.disconnect();
     setUser({ userId: '', username: '', password: '', login: false });
     logoutStorage();
   };
+
+  const handleLoadMsgGroup = (groupId, groupMessages) => {
+    setCurrentGroupId(groupId);
+    setMessages(groupMessages);
+  }
+
+  const handleToggleGroupModal = (mode) => {
+    setGroupModelMode(mode);
+    setShowGroupModal(!showGroupModal); // Mostrar o ocultar el popup de nuevo grupo o unión a grupo
+  }
+
+  // Función para crear un nuevo grupo o unirse a uno existente
+  const handleGroupAction = (groupName, groupPassword, action) => {
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa', action)
+    if (action === 'create') {
+      // Crear nuevo grupo
+      console.log('Creando nuevo grupo:', groupName, 'con contraseña:', groupPassword);
+      socket.current.emit('create group', groupName, groupPassword, userId);
+      // Lógica para crear el grupo...
+    } else if (action === 'join') {
+      // Unirse a grupo existente
+      console.log('Uniéndose al grupo:', groupName, 'con contraseña:', groupPassword);
+      socket.current.emit('join group', groupName, groupPassword, userId);
+    }
+    setShowGroupModal(false);
+  }
+
+  const handleLeaveGroup = (groupId) => {
+    socket.current.emit('leave group', groupId, userId);
+    let groupMessagesEdit = groupMessages.filter(group => group.groupId !== groupId);
+    setGroupMessages(groupMessagesEdit);
+    if (groupId === currentGroupId) {
+      setMessages([]);
+    }
+
+
+  }
 
   return (
     <>
@@ -51,26 +137,37 @@ const App = () => {
           <Button title="Cerrar sesión" onPress={handleLogout} />
         </View>
         <View>
-          
+          <View>
+            {groupMessages.length === 0 ? <Text>No hay mensajes</Text> : (
+              groupMessages.map((group, index) => {
+                console.log("groupMessages2", groupMessages)
+                return (
+                  <View key={group.groupId} style={styles.messagesContainer}>
+                    <Button title={group.groupName} onPress={() => handleLoadMsgGroup(group.groupId, group.messages)} />
+                    <Button title="Leave Group" onPress={() => handleLeaveGroup(group.groupId)} />
+
+                  </View>
+                );
+              })
+            )}
+            <Button title="Join group" onPress={() => handleToggleGroupModal('join')} />
+            <Button title="Create group" onPress={() => handleToggleGroupModal('create')} />
+          </View>
 
           <View style={styles.chatContainer}>
             <ScrollView contentContainerStyle={styles.scrollContent}>
-              {groupMessages.length === 0 ? <Text>No hay mensajes</Text> : (
-                groupMessages[0].msg.map((group, index) => {
+              {messages.length === 0 ? <Text>No hay mensajes</Text> : (
+                messages.map((message, index) => {
+                  console.log("messages", messages)
                   return (
-                    group.map((message, index) => {
-
-                      return (
-                        <View key={message.id} style={[styles.message, message.users.username === username && styles.myMessage]}>
-                          <View>
-                            <Text>{message.users.username}</Text>
-                          </View>
-                          <View style={styles.msgContainer}>
-                            <Text>{message.content}</Text>
-                          </View>
-                        </View>
-                      );
-                    })
+                    <View key={message.id} style={[styles.message, message.users.username === username && styles.myMessage]}>
+                      <View>
+                        <Text>{message.users.username}</Text>
+                      </View>
+                      <View style={styles.msgContainer}>
+                        <Text>{message.content}</Text>
+                      </View>
+                    </View>
                   );
                 })
               )}
@@ -87,6 +184,13 @@ const App = () => {
           </View>
         </View>
       </View>
+      {/* Popup para crear nuevo grupo o unirse a uno */}
+      <GroupModal
+        visible={showGroupModal}
+        onClose={handleToggleGroupModal}
+        onSubmit={handleGroupAction}
+        mode={groupModelMode}
+      />
     </>
   );
 };
@@ -97,8 +201,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#121212',
     padding: 36,
     alignItems: 'center',
-
-
   },
   chatContainer: {
     flex: 1,
@@ -118,6 +220,10 @@ const styles = StyleSheet.create({
   },
   messagesContainer: {
     width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
   },
   msgContainer: {
     flex: 1,
